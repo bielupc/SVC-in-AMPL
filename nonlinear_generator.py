8993d983d83b07c93c350b7ec60e9ffe950cf2fc
laiamp <laiamogasp@gmail.com>
2024-05-19 12:36:05 +0200

    nonlinear swiss-roll

---
+++ nonlinear_generator.py
@@ -0,0 +1,33 @@
+from sklearn.datasets import make_swiss_roll
+import numpy as np
+
+
+
+FILENAME = "nonlinear.dat"
+
+def save_dataset(points, labels, filename) -> None:
+    assert len(points) == len(labels)
+    # Crear y escribir en el archivo .dat
+    with open(filename, "w") as file:
+        for point, label in zip(points, labels):
+            file.write(" ".join(map(str, point)) + " " + str(label) + "\n")
+
+
+def generate_swiss(size=400):
+    x, t = make_swiss_roll(n_samples=size)
+    labels = np.where(t < np.mean(t), 1, -1)    # values below mean or median of t are a class
+    return x, labels
+
+
+def main():
+    inp = input("Enter size of dataset: ")
+    if len(inp) == 0:
+        x, labels = generate_swiss()
+    else:
+        x, labels = generate_swiss(int(inp))
+
+    save_dataset(x, labels, FILENAME)
+        
+
+if __name__ == "__main__":
+    main()
