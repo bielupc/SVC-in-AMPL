f8ff92b401b287453f8e59147d03b62a8cb88d85
laiamp <laiamogasp@gmail.com>
2024-05-24 20:22:43 +0200

    graphic

--- scripts/nonlinear_generator.py
+++ scripts/nonlinear_generator.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import make_swiss_roll
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 FILENAME = "nonlinear.dat"
@@ -18,6 +19,23 @@ def generate_swiss(size=400):
     return x, labels
 
 
+def plot_swiss(x, labels):
+        # Crear la figura y el eje 3D
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Graficar los puntos del dataset
+    ax.scatter(x[:, 0], x[:, 1], x[:, 2], c=labels, cmap=plt.cm.Spectral)
+
+    # Añadir etiquetas y título
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    # Mostrar el gráfico
+    plt.show()
+
+
 def main():
     inp = input("Enter size of dataset: ")
     if len(inp) == 0:
@@ -25,7 +43,8 @@ def main():
     else:
         x, labels = generate_swiss(int(inp))
 
-    save_dataset(x, labels, FILENAME)
+    plot_swiss(x, labels)
+    #save_dataset(x, labels, FILENAME)
         
 
 if __name__ == "__main__":
